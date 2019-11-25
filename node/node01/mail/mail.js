b32e4869866ddaf10a5bb89ccdd1f6ecf773413a
Apple <apple@AppledeMacBook-Pro.local>
2019-11-25 16:03:05 +0800

    node01-pm

--- node/node01/mail/mail.js
+++ node/node01/mail/mail.js
@@ -13,7 +13,7 @@ const nodemailer = require("nodemailer");
     secure: true, // true for 465, false for other ports
     auth: {
       user: '352186537@qq.com', // 邮箱账号
-      pass: 'xkdkiwrxxbpnbhhb' // 邮箱smtp验证码
+      pass: 'hexkdkiwrxxbpnbhhb' // 邮箱smtp验证码
     }
   });
  let img='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1574676855352&di=af619ea971a2d91d18a36b2026faf5bd&imgtype=0&src=http%3A%2F%2Fphotocdn.sohu.com%2F20120614%2FImg345602334.jpg'
